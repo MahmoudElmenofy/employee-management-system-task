5a190039ccb077b27cc133104ec9e5f93bf5d61a
MahmoudElmenofy <elmenofy45@gmail.com>
2025-05-16 17:39:35 +0300

    # add new employee to sql script

--- employee-management-system/src/main/resources/data.sql
+++ employee-management-system/src/main/resources/data.sql
@@ -4,4 +4,5 @@ INSERT INTO employee (first_name, last_name, email, department, salary) VALUES
 ('mohamed', 'kamal', 'mohamed.kamal@gmail.com', 'Marketing', 45000.00),
 ('karim', 'mohamed', 'karim.mohamed@gmail.com', 'Engineering', 60000.00),
 ('youssef', 'ahmed', 'youssef.ahmed@gmail.com', 'Human Resources', 40000.00),
-('nermin', 'mosaad', 'nermin.mmosaad@gmail.com', 'Sales', 35000.00);
+('nermin', 'mosaad', 'nermin.mmosaad@gmail.com', 'Sales', 35000.00),
+('marwa', 'makram', 'marwa.makram@gmail.com', 'Sales', 35000.00);
